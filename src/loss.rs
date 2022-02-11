2a3573f4954904f63b905f27b3554c5c77238c19
ikanago <ikanago-dev@protonmail.com>
2022-02-11 21:15:56 +0900

    Implement softmax cross entropy

--- src/loss.rs
+++ src/loss.rs
@@ -1,4 +1,4 @@
-use ndarray::{Array1, Array2, ArrayView2, Zip};
+use ndarray::{Array1, Array2, ArrayView1, ArrayView2, Zip};
 
 pub trait LossCriterion {
     fn compute(input: ArrayView2<f64>, target: ArrayView2<f64>) -> Self;
@@ -38,13 +38,11 @@ impl LossCriterion for MeanSquaredError {
 
     fn grad(&self) -> Array2<f64> {
         let n = self.input.len();
-        let mut grad = &self.input - &self.target;
-        grad /= n as f64;
-        grad
+        (&self.input - &self.target) / n as f64
     }
 }
 
-fn softmax(x: &Array1<f64>) -> Array1<f64> {
+fn softmax(x: ArrayView1<f64>) -> Array1<f64> {
     let max_element = x.iter().fold(f64::NAN, |v, &w| v.max(w));
     let exp_each = x.map(|v| (v - max_element).exp());
     let exp_sum = exp_each.sum();
@@ -57,15 +55,37 @@ pub struct SoftmaxCrossEntropy {
     value: f64,
 }
 
-/*
 impl LossCriterion for SoftmaxCrossEntropy {
     fn compute(input: ArrayView2<f64>, target: ArrayView2<f64>) -> Self {
-        let loss = Zip::from(input.rows()).and(target.rows()).fold(0.0, |loss, input, target| {
-            loss + target + input
-        });
+        let batch_size = input.nrows();
+        let loss =
+            Zip::from(input.rows())
+                .and(target.rows())
+                .fold(0.0, |batch_loss, input, target| {
+                    let softmax = softmax(input.view());
+                    batch_loss
+                        + Zip::from(&target)
+                            .and(&softmax)
+                            .fold(0.0, |loss, t, y| loss + t * y.log2())
+                })
+                / (batch_size as f64 * -1.0);
+
+        Self {
+            input: input.to_owned(),
+            target: target.to_owned(),
+            value: loss,
+        }
+    }
+
+    fn value(&self) -> f64 {
+        self.value
+    }
+
+    fn grad(&self) -> Array2<f64> {
+        let batch_size = self.input.nrows();
+        (&self.input - &self.target) / batch_size as f64
     }
 }
-*/
 
 #[cfg(test)]
 mod tests {
@@ -112,7 +132,25 @@ mod tests {
                 0.0432699564108081,
                 0.7115591489765052
             ]),
-            softmax(&x)
+            softmax(x.view())
         );
+    }
+
+    fn prepare_softmax_cross_entropy() -> SoftmaxCrossEntropy {
+        let input = arr2(&[[1.0, 0.5, -0.1], [0.5, 0.2, 3.0]]);
+        let target = arr2(&[[1.0, 0.0, 0.0], [1.0, 0.0, 0.0]]);
+        SoftmaxCrossEntropy::compute(input.view(), target.view())
+    }
+
+    #[test]
+    fn compute_softmax_cross_entropy() {
+        let loss = prepare_softmax_cross_entropy();
+        assert_relative_eq!(2.3775211159596603, loss.value());
+    }
+
+    #[test]
+    fn softmax_cross_entropy_grad() {
+        let loss = prepare_softmax_cross_entropy();
+        assert_rel_eq_arr2!(loss.grad(), arr2(&[[0.0, 0.25, -0.05], [-0.25, 0.1, 1.5]]));
     }
 }
