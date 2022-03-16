8a6f853dd21a18a7342ba223c723ac05e52c0b12
ikanago <ikanago-dev@protonmail.com>
2022-03-16 10:30:03 +0900

    Implement broadcast_zero

--- src/grad.rs
+++ src/grad.rs
@@ -11,9 +11,11 @@ use std::{
     rc::Rc,
 };
 
-use ndarray::{Array, ArrayView, Dimension, IntoNdProducer, Zip};
+use ndarray::{Array, ArrayView, DimMax, Dimension, IntoNdProducer, Zip};
 
 pub type Tensor<D> = Array<f32, D>;
+pub(crate) type Broadcasted<Lhs, Rhs> = <Lhs as DimMax<Rhs>>::Output;
+pub(crate) type BroadTensor<Lhs, Rhs> = Tensor<Broadcasted<Lhs, Rhs>>;
 
 /// Trait to represent a computational graph of a function to be diffrentiated.
 /// All node in the graph implements this trait.
@@ -132,4 +134,107 @@ impl<D: Dimension> Function for Variable<D> {
     fn forward(&self) {}
 
     fn backward(&self) {}
+}
+
+pub(crate) fn broadcast_zeros<Lhs, Rhs>(
+    lhs: &Tensor<Lhs>,
+    rhs: &Tensor<Rhs>,
+) -> BroadTensor<Lhs, Rhs>
+where
+    Lhs: Dimension + DimMax<Rhs>,
+    Rhs: Dimension,
+{
+    let (larger_shape, smaller_shape) = if lhs.ndim() > rhs.ndim() {
+        (lhs.shape(), rhs.shape())
+    } else {
+        (rhs.shape(), lhs.shape())
+    };
+    let leading_dims = larger_shape.len() - smaller_shape.len();
+
+    let mut broadcasted_shape = Broadcasted::<Lhs, Rhs>::zeros(larger_shape.len());
+    let broadcasted_shape_iter = broadcasted_shape.slice_mut().iter_mut().rev();
+    let smaller_shape_iter = smaller_shape
+        .iter()
+        .rev()
+        .chain([1].iter().cycle().take(leading_dims))
+        .collect::<Vec<_>>();
+    larger_shape
+        .iter()
+        .rev()
+        .zip(smaller_shape_iter)
+        .zip(broadcasted_shape_iter)
+        .for_each(|((&l, &r), res)| {
+            *res = if l == r {
+                l
+            } else if l == 1 {
+                r
+            } else if r == 1 {
+                l
+            } else {
+                panic!("Shape does not met: {:?}, {:?}", lhs.shape(), rhs.shape());
+            }
+        });
+
+    Tensor::zeros(broadcasted_shape)
+}
+
+#[cfg(test)]
+mod tests {
+    use super::*;
+
+    #[test]
+    fn same_shape() {
+        let a = Tensor::zeros((2, 3, 4));
+        let b = Tensor::zeros((2, 3, 4));
+        let x = broadcast_zeros(&a, &b);
+        assert_eq!(&[2, 3, 4], x.shape());
+    }
+
+    #[test]
+    fn one_of_dim_has_one() {
+        let a = Tensor::zeros((2, 3, 4));
+        let b = Tensor::zeros((2, 1, 4));
+        let x = broadcast_zeros(&a, &b);
+        assert_eq!(&[2, 3, 4], x.shape());
+    }
+
+    #[test]
+    fn some_of_dim_has_one_for_a_shape() {
+        let a = Tensor::zeros((2, 3, 4));
+        let b = Tensor::zeros((2, 1, 1));
+        let x = broadcast_zeros(&a, &b);
+        assert_eq!(&[2, 3, 4], x.shape());
+    }
+
+    #[test]
+    fn some_of_dim_has_one_for_either_shape() {
+        let a = Tensor::zeros((2, 1, 4, 1));
+        let b = Tensor::zeros((2, 1, 1, 5));
+        let x = broadcast_zeros(&a, &b);
+        assert_eq!(&[2, 1, 4, 5], x.shape());
+    }
+
+    #[test]
+    fn one_of_shape_lacks_one_dim() {
+        let a = Tensor::zeros((2, 3, 4));
+        let b = Tensor::zeros((1, 4));
+        let x = broadcast_zeros(&a, &b);
+        assert_eq!(&[2, 3, 4], x.shape());
+    }
+
+    #[test]
+    #[should_panic]
+    fn one_of_dim_is_different_non_one() {
+        let a = Tensor::zeros((2, 3, 4));
+        let b = Tensor::zeros((2, 2, 4));
+        broadcast_zeros(&a, &b);
+    }
+
+    #[test]
+    #[should_panic]
+    fn one_of_dim_is_different_non_one_and_one_of_shape_lacks_one_dim() {
+        let a = Tensor::zeros((2, 3, 4));
+        let b = Tensor::zeros((2, 4));
+        broadcast_zeros(&a, &b);
+    }
 }
