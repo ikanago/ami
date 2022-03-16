0add137582aad3f4b1d3a9fc268df7863f21f513
ikanago <ikanago-dev@protonmail.com>
2022-03-16 17:33:20 +0900

    Define `Model`, `Linear` and `Compose`

---
+++ src/model.rs
@@ -0,0 +1,113 @@
+use std::marker::PhantomData;
+
+use ndarray::{Array, Ix1, Ix2};
+use ndarray_rand::{rand_distr::Uniform, RandomExt};
+
+use crate::grad::{
+    addition::{add, Addition},
+    matmul::{matmul, MatrixMultiplication},
+    Function, Variable,
+};
+
+/// Trait to represent learning model.
+pub trait Model<In, Out>
+where
+    In: Function,
+    Out: Function,
+{
+    fn forward(&self, input: In) -> Out;
+}
+
+/// Linear transformation layer.
+pub struct Linear {
+    weight: Variable<Ix2>,
+    bias: Variable<Ix1>,
+}
+
+impl Linear {
+    pub fn new(in_features: usize, out_features: usize) -> Self {
+        let weight = Variable::new(Array::random(
+            (in_features, out_features),
+            Uniform::new(-1.0, 1.0),
+        ))
+        .requires_grad();
+        let bias =
+            Variable::new(Array::random((out_features,), Uniform::new(-1.0, 1.0))).requires_grad();
+
+        Self { weight, bias }
+    }
+}
+
+impl<In> Model<In, Addition<MatrixMultiplication<In, Variable<Ix2>>, Variable<Ix1>>> for Linear
+where
+    In: Function<Dim = Ix2, GradDim = Ix2>,
+{
+    fn forward(
+        &self,
+        input: In,
+    ) -> Addition<MatrixMultiplication<In, Variable<Ix2>>, Variable<Ix1>> {
+        add(&matmul(&input, &self.weight), &self.bias)
+    }
+}
+
+pub struct Compose<F, S, In, Mid, Out>
+where
+    F: Model<In, Mid>,
+    S: Model<Mid, Out>,
+    In: Function,
+    Mid: Function,
+    Out: Function,
+{
+    first: F,
+    second: S,
+    _in: PhantomData<In>,
+    _mid: PhantomData<Mid>,
+    _out: PhantomData<Out>,
+}
+
+/// Compose two models.
+/// Incoming data is applied to `first`, then applied to `second`.
+pub fn compose<F, S, In, Mid, Out>(first: F, second: S) -> Compose<F, S, In, Mid, Out>
+where
+    F: Model<In, Mid>,
+    S: Model<Mid, Out>,
+    In: Function,
+    Mid: Function,
+    Out: Function,
+{
+    Compose {
+        first,
+        second,
+        _in: PhantomData,
+        _mid: PhantomData,
+        _out: PhantomData,
+    }
+}
+
+impl<F, S, In, Mid, Out> Model<In, Out> for Compose<F, S, In, Mid, Out>
+where
+    F: Model<In, Mid>,
+    S: Model<Mid, Out>,
+    In: Function,
+    Mid: Function,
+    Out: Function,
+{
+    fn forward(&self, input: In) -> Out {
+        let mid = self.first.forward(input);
+        self.second.forward(mid)
+    }
+}
+
+#[cfg(test)]
+mod tests {
+    use super::*;
+
+    use ndarray::arr2;
+
+    #[test]
+    fn forward_compose() {
+        let model = compose(Linear::new(2, 4), Linear::new(4, 1));
+        let x = Variable::new(arr2(&[[1.0, 0.5], [2.0, 3.0]]));
+        model.forward(x);
+    }
+}
