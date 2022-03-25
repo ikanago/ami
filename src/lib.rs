679bafe4a5bba5688868ee431860ac7cd646c806
ikanago <ikanago-dev@protonmail.com>
2022-03-25 17:25:19 +0900

    Decode one-hot vector to label

--- src/lib.rs
+++ src/lib.rs
@@ -1,6 +1,6 @@
 use std::{collections::HashMap, hash::Hash};
 
-use ndarray::Array2;
+use ndarray::{Array2, Axis};
 
 pub mod activation;
 pub mod data;
@@ -35,40 +35,76 @@ macro_rules! assert_rel_eq_arr2 {
     };
 }
 
+/// Encode labels to one-hot vectors and decode them.
 pub struct OneHotEncoder<Label>
 where
-    Label: Hash + Eq,
+    Label: Hash + Eq + Clone,
 {
     label_to_id: HashMap<Label, usize>,
+    id_to_label: Vec<Label>,
 }
 
 impl<Label> OneHotEncoder<Label>
 where
-    Label: Hash + Eq,
+    Label: Hash + Eq + Clone,
 {
+    /// Record labels to convert.
     pub fn new(label_kinds: Vec<Label>) -> Self {
         let label_to_id = label_kinds
-            .into_iter()
+            .iter()
+            .cloned()
             .enumerate()
             .map(|(id, label)| (label, id))
             .collect();
-        Self { label_to_id }
+        Self {
+            label_to_id,
+            id_to_label: label_kinds,
+        }
     }
 
     fn encode_label(&self, label: &Label) -> Vec<f32> {
-        let id = self.label_to_id.get(&label).expect("Unknown label");
+        let id = self.label_to_id.get(label).expect("Unknown label");
         let mut one_hot = vec![0.0; self.label_to_id.len()];
         one_hot[*id] = 1.0;
         one_hot
     }
 
+    /// Encode labels to one-hot vectors as 2D matrix whose shape is (n_data, n_features).
+    /// Panics if an unknown label is passed.
     pub fn encode(&self, labels: &[Label]) -> Array2<f32> {
         let one_hot_vecs = labels
-            .into_iter()
+            .iter()
             .map(|label| self.encode_label(label))
             .flatten()
             .collect();
         Array2::from_shape_vec((labels.len(), self.label_to_id.len()), one_hot_vecs).unwrap()
+    }
+
+    /// Decode one-hot vectors to labels.
+    /// Decoded label is determined by an argmax of each one-hot vector.
+    pub fn decode(&self, one_hot_vecs: Array2<f32>) -> Vec<Label> {
+        one_hot_vecs
+            .lanes(Axis(1))
+            .into_iter()
+            .map(|one_hot| {
+                // argmax of `one_hot`
+                one_hot
+                    .into_iter()
+                    .enumerate()
+                    .fold(
+                        (0, 0.0f32),
+                        |(max_index, max_elem), (index, &one_hot_elem)| {
+                            if one_hot_elem > max_elem {
+                                (index, one_hot_elem)
+                            } else {
+                                (max_index, max_elem)
+                            }
+                        },
+                    )
+                    .0
+            })
+            .map(|id| self.id_to_label[id].clone())
+            .collect()
     }
 }
 
@@ -100,6 +136,28 @@ mod tests {
                 [0.0, 0.0, 1.0,],
             ]),
             one_hot_vecs
+        );
+    }
+
+    #[test]
+    fn decode_one_hot() {
+        let label_kinds = vec!["A", "B", "C"].into_iter().map(String::from).collect();
+        let encoder = OneHotEncoder::new(label_kinds);
+
+        let one_hot_vecs = arr2(&[
+            [1.0, 0.0, 0.0],
+            [0.8, 0.2, 0.0],
+            [0.05, 0.05, 0.9],
+            [0.0, 1.0, 0.0],
+            [0.1, 0.2, 0.7],
+        ]);
+        let labels = encoder.decode(one_hot_vecs);
+        assert_eq!(
+            vec!["A", "A", "C", "B", "C"]
+                .into_iter()
+                .map(String::from)
+                .collect::<Vec<_>>(),
+            labels
         );
     }
 }
